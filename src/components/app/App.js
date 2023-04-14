e9c0e2733b1f873ac5ee1065639ddaa8864517f9
Alexey <monticorik@gmail.com>
2023-04-14 22:34:26 +0300

    Add Routes
    divided the project into pages. made index.js that return pages components. added routes in app.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,39 +1,22 @@
-import { useState } from "react";
-import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ComicsList from "../comicsList/ComicsList";
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import decoration from '../../resources/img/vision.png';
+import AppHeader from "../appHeader/AppHeader";
+import {MainPage, ComicsPage} from "../pages";
 
 const App = () => {
-    const [charId, setCharId] = useState(null);
-
-    const onCharUpdate = (id) => {
-        setCharId(id);
-    }
     
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {/* <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharUpdate={onCharUpdate}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={charId}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-            <ComicsList/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>} />
+                        <Route path="/comics" element={<ComicsPage/>} />
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     )
 }
 
